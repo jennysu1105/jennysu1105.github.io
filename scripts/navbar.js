7ed9ab220bf6cd91d11515b2b984f0ab5ffcc93b
Jenny Su <71398958+jennysu1105@users.noreply.github.com>
2023-04-06 21:17:31 -0400

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -7,14 +7,14 @@ class Navigation extends HTMLElement {
         this.innerHTML = `
         <nav class="navbar navbar-expand-sm navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Jenny Su</a>
+                <a class="navbar-brand" href="/">Jenny Su</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarText">
                     <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">About Me</a>
+                        <a class="nav-link" href="/">About Me</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/resume.html">Resume</a>
